6a5bd9842437630e4427f4c7c3dc79b9d908d19e
Mario Blažeka <147752707+marioblazeka@users.noreply.github.com>
2026-03-05 19:05:20 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function showTime() {
     const now = new Date();
-    const formatted = now.toLocaleTimeString("hr-HR", {
+
+    const formattedTime = now.toLocaleTimeString("hr-HR", {
       timeZone: "Europe/Zagreb",
       hour: "2-digit",
       minute: "2-digit",
       second: "2-digit"
     });
 
-    document.getElementById("currentTime").textContent = formatted;
+    document.getElementById("currentTime").textContent = formattedTime;
   }
 
   showTime();
   setInterval(showTime, 1000);
-
-  console.log("radi", new Date());
 
 });
