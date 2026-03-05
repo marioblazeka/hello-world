581eb9bf1ee7a5c47ecad8a1b6fef320ce458914
Mario Blažeka <147752707+marioblazeka@users.noreply.github.com>
2026-03-05 17:39:31 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,17 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML =
-		new Date().toLocaleString("hr-HR", { timeZone: "Europe/Zagreb" });
-}
+document.addEventListener("DOMContentLoaded", function () {
+
+  function showTime() {
+    const element = document.getElementById("currentTime");
+    if (!element) {
+      console.log("Element nije pronađen");
+      return;
+    }
+
+    element.textContent =
+      new Date().toLocaleString("hr-HR", { timeZone: "Europe/Zagreb" });
+  }
+
+  showTime();
+  setInterval(showTime, 1000);
+
+});
